c6e5f157eabd8c50999ec7c2c9650d3e67688421
EvilExtractor13 <SadlerA27@chcstudent.com>
2025-09-12 12:28:38 -0400

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,10 @@
+marth = input("enter a number ")
+bower = input("enter the second number ")
+bower = int(bower)
+marth = int(marth)
+sum = marth + bower
+print(f"the answer is {sum}")
+marth2 = input("enter a number to subtract ")
+bower2 = input("enter another number ")
+sum = marth2 - bower2
+print(f"the answer is {sum}")
