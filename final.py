696c87ba4c0d96815903e85eb77d4595e792cfc4
Katakind <Mitzimaster23@gmail.com>
2025-09-24 14:36:44 -0400

    Update final.py

--- final.py
+++ final.py
@@ -1,3 +1,11 @@
-exit == 1
-input("put any number you want and watch it turn to 1! ")
-if 
+x = input("put any number you want and watch it turn to 1! ")
+x = int(x)
+check = True
+while check == True:
+    print(x)
+    if x == 1:
+        check = False 
+    if x % 2 == 0:
+        x= x / 2
+    elif x % 2 == 1:
+        x = x * 3 + 1
