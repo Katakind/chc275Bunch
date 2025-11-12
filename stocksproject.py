186d8a23bb3c7da6a6532d76bdc7d22a752f6e14
Katakind <SadlerA27@chcstudent.com>
2025-11-11 19:16:20 -0500

    Update stocksproject.py

--- stocksproject.py
+++ stocksproject.py
@@ -1,10 +1,31 @@
 firstdays = open("stocksdays", "r")
-buffer1 = firstdays.readlines() 
+buffer1 = firstdays.readlines() [0]
 firstdays.close()
 print(buffer1)
 
-lastdays = open("stockdays2", "r")
-buffer2 = lastdays.readlines()
+firstdays = open("stocksdays", "r")
+buffer1 = firstdays.readlines() [1]
+firstdays.close()
+print(buffer1)
+
+firstdays = open("stocksdays", "r")
+buffer1 = firstdays.readlines() [2]
+firstdays.close()
+print(buffer1)
+
+lastdays = open("stocksdays2", "r") 
+buffer2 = lastdays.readlines() [0]
 lastdays.close()
 print(buffer2)
 
+lastdays = open("stocksdays2", "r") 
+buffer2 = lastdays.readlines() [1]
+lastdays.close()
+print(buffer2)
+
+lastdays = open("stocksdays2", "r") 
+buffer2 = lastdays.readlines() [2]
+lastdays.close()
+print(buffer2)
+
+mean = sum/len 
