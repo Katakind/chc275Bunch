aad517ae432fbf514b8d9451bc10bcb69c7c0f70
NeffJ27 <neffj27@chcstudent.com>
2025-11-20 13:56:13 -0500

    Create Foodstore.py

---
+++ Foodstore.py
@@ -0,0 +1,19 @@
+exitcondition = False 
+while exitcondition == False:
+    print("Welcome to the grocery store of doom and despair")
+    print("1. add to cart")
+    print("2. remove from cart")
+    print("3. check out")
+    input("select an option ")
+
+if 1
+
+
+
+
+
+
+total = 0
+food = []
+prices = []
+
