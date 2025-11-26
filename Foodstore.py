5992afad80a9146d2ff80aa435ba59a50535d205
NeffJ27 <neffj27@chcstudent.com>
2025-11-26 10:19:35 -0500

    Update Foodstore.py

--- Foodstore.py
+++ Foodstore.py
@@ -1,19 +1,21 @@
 exitcondition = False 
+foodlist = []
+carts = []
+quanity = []
+cartprice = []
 while exitcondition == False:
     print("Welcome to the grocery store of doom and despair")
     print("1. add to cart")
     print("2. remove from cart")
     print("3. check out")
-    input("select an option ")
-
-if 1
-
-
-
-
-
-
-total = 0
-food = []
-prices = []
-
+    storesystem = input("select an option ")
+    
+    if storesystem == "add to cart":
+        print(foodlist)
+        foodselection = input("Choose which you want")
+    
+    
+    elif storesystem == "remove from cart":
+        print(carts) 
+        input("select which to remove")
+       
