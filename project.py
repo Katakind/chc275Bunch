65ea07c0c67c2ed0c0f55b2f94313424b1983076
NeffJ27 <82914861+Katakind@users.noreply.github.com>
2025-12-03 13:58:52 -0500

    Update project.py

--- project.py
+++ project.py
@@ -5,13 +5,15 @@ pygame.init()
 screen = pygame.display.set_mode((1920, 1080))
 
 courageboy_img = pygame.image.load('courageboy.jpg').convert()
+Textbox = pygame.image.load('Persona.png').convert()
+
 
 running = True
 x = 0
 while running:
     
     screen.blit(courageboy_img, (x, 30))
-   
+    screen.blit(Textbox, (300,600))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
