0a1880ab718fadcfbcb697a18af4fb49e1405156
Katakind <Mitzimaster23@gmail.com>
2025-12-14 14:26:20 -0500

    w

--- project.py
+++ project.py
@@ -35,33 +35,34 @@ clock = pygame.time.Clock()
 FONT = pygame.font.SysFont("cabin", 24)
 COLOR = (255,255,255)
 running = True
-x = 0
-i = 0
+currentline = 0
 #textbox
 Textbox = pygame.image.load('Textboxofdoom.png').convert()
 
 
 
-#clicking function
+
 while running:
-    text = FONT.render(f"{buffer[i]}",True,COLOR)
+    if currentline >= len(buffer):
+        currentline = 0
+    text = FONT.render(f"{buffer[currentline]}",True,COLOR)
+   
+#blit
     screen.blit(Background_img, (0,0))
     screen.blit(Textbox, (1000, 800))
     screen.blit(text  ,(100,100))
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:   
-              i += 1
+              currentline += 1
 #fps
     pygame.display.flip()
     clock.tick(60)
-#loop
-    if i ==len(buffer):
-        i = 0
 
 
 
-#quit function
-pygame.quit()            
+pygame.quit()        
+
