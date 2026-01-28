6061e46d960351bad74d8255a118dab7317e5596
Katakind <Mitzimaster23@gmail.com>
2026-01-28 12:21:52 -0500

    Create drill2.py

---
+++ drill2.py
@@ -0,0 +1,8 @@
+while True:
+    def makelist():
+        numbersofdoom = []
+        worth = input("Enter a number or type stop and it will stop...crazy how that works")
+        if worth == "stop":
+            break
+    list1 = makelist()        
+    list2 = makelist()
