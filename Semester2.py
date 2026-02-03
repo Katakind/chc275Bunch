d1070a3ef825bb8b88ebcf74e2b5f4d29fb8742b
NeffJ27 <neffj27@chcstudent.com>
2026-02-03 12:55:49 -0500

    Update Semester2.py

--- Semester2.py
+++ Semester2.py
@@ -47,10 +47,62 @@ def add7(x):
     x = x + 7
     return x #this is going to flag python to remember that x got reassigned to x + 7
 
-
 def add(x, y):
     print(x + y) #DONT DO THIS
     
 def add(x, y):
     return x + y #do this
 
+
+
+#we want a file layout that is (1.) readable (2.) Exposes all of the user implemented functions right as the intereperter starts executing code.
+    #exposed = the functon is able to be called
+    
+#here are the requirements for software engineers:
+#1. all of the functions to be exposed to us
+#2. dont want code being executed inline with function definitions
+
+"""
+How should we lay out our file when we start a project
+    -function definitions at the top
+    -code to be evaluated at the bottom
+    
+We can think of functions as services that we can use at the bottom of our file.
+"""
+#We want to introduce a new structure that is relevant to pretty much all other coding languages. This structure is called the main function!
+
+def foo():
+    print("bar")
+
+def fizz(x):
+    if x % 2 == 1: #checks if x is even or odd
+        print("buzz")
+#-----------------------------------------------
+def main(): #main is a special kind of function, this only gets evaluated if the parent file is the being exectured
+            #he will expand on this later
+    check = True
+    while check == True:
+        print("welcome to the test program")
+        print("1. foo")
+        print("2. fizz")
+        print("type exit to quit")
+        option = input("Enter an option: ")
+        if option == "1":
+            foo()
+        if option == "2":
+            number = input("wgar is your number? ")
+            try:
+                number = int(number)
+            except Exception as e:
+                print(e)
+            else:
+                fizz(number)
+            if option == "quit":
+                check = False
+                
+"""
+to call the main function, we need to do a convoluted process in an if statement
+"""
+
+if __name__ == "__main__":
+    main()
